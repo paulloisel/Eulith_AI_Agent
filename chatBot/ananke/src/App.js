1af20efc31672d835ffbe1fe0a8d49ed49b4d5cb
paulloisel <107944574+paulloisel@users.noreply.github.com>
2023-05-25 08:24:07 -0700

    Ananke_software_part

--- chatBot/ananke/src/App.js
+++ chatBot/ananke/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <section className="side-bar">
+        <button>+ New chat</button>
+        <ul className="history">
+          <li>BUGH</li>
+        </ul>
+        <nav>
+          <p>Made by Paul</p>
+        </nav>
+      </section>
+      <section className="main">
+        <h1>Ananke</h1>
+        <ul className="feed">
+
+        </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input/>
+            <div id="submit">➢</div>
+          </div>
+          <p className="info">
+            Powered by OpenAI API based on ChatGPT May 12 Version
+          </p>
+        </div>
+      </section>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
