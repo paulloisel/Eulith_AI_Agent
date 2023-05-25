ffc44631336f1edee7f6b056df9b4c22f585afaa
paulloisel <107944574+paulloisel@users.noreply.github.com>
2023-05-25 12:25:24 -0700

    message_value_work

--- chatBot/ananke/src/App.js
+++ chatBot/ananke/src/App.js
@@ -1,4 +1,30 @@
+import { useState, useEffect} from "react"
+
+
 const App = () => {
+  const [value, setValue] = useState(null)
+  const [message, setMessage] = useState(null)
+
+  const getMessages = async () => {
+    const options = {
+      method: "POST",
+      body: JSON.stringify({
+        message: value
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+    try {
+      const response = await fetch('http://localhost:8000/completions', options)
+      const data = await response.json()
+      setMessage(data.choices[0].message)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  console.log(message)
   return (
     <div className="app">
       <section className="side-bar">
@@ -17,8 +43,8 @@ const App = () => {
         </ul>
         <div className="bottom-section">
           <div className="input-container">
-            <input/>
-            <div id="submit">➢</div>
+            <input value={value} onChange={(e) => setValue(e.target.value)}/>
+            <div id="submit" onClick={getMessages}>➢</div>
           </div>
           <p className="info">
             Powered by OpenAI API based on ChatGPT May 12 Version
